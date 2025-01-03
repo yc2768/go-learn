5f37d0600d33528185d0cbe93da963ecf76664a0
v_pycopeng <v_pycopeng@tencent.com>
2025-01-03 11:01:58 +0800

    格式化输出

--- main.go
+++ main.go
@@ -5,5 +5,13 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello Go!")
+	//字符拼接
+	fmt.Println("Hello" + " Go!")
+
+	var stockcode = 123
+	var enddate = "2025.01.02"
+	var url = "Code=%d&enddate=%s"
+	var target_url = fmt.Sprintf(url, stockcode, enddate)
+	fmt.Println(target_url)
+
 }
